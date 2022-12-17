bd22e168b066f7296dc59e738ae88056e099253c
“RumitPatel” <rumit.patel5@gmail.com>
2022-12-17 18:54:16 +0530

    Called function from library.

--- app/src/main/java/com/rumitpatel/medicinereminderdemo/MainActivity.kt
+++ app/src/main/java/com/rumitpatel/medicinereminderdemo/MainActivity.kt
@@ -2,13 +2,17 @@ package com.rumitpatel.medicinereminderdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.rumitpatel.medicinereminder.MyUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    fun testFunction(v:View) {
         MyUtils.showTestToast(this, "my message")
     }
 }
