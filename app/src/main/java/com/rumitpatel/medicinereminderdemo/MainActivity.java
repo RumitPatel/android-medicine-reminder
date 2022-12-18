f947504b975a139cc09759aa6bac33aed54adce3
“RumitPatel” <rumit.patel5@gmail.com>
2022-12-18 11:23:30 +0530

    library_java_1 added.

---
+++ app/src/main/java/com/rumitpatel/medicinereminderdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.rumitpatel.medicinereminderdemo;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rumitpatel.medicinereminder.AppUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view) {
+        AppUtils appUtils = new AppUtils();
+        appUtils.showToast(this, "My message");
+    }
+}
